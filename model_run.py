824325461022a9a8ee95f8d164e194a6e94839cf
Gunjan Mohan <gunjanx1997@gmail.com>
2018-11-02 23:10:37 +0530

    chal gya :P

--- model_run.py
+++ model_run.py
@@ -10,7 +10,7 @@ weights = os.path.join(model_root, 'VGG_CNN_S.caffemodel')
 assert os.path.exists(weights)
 solver = caffe.get_solver(model_root+'solver.prototxt')
 solver.net.copy_from(weights)
-niter = 200000
+niter = 550
 solver.step(niter)
 weight_dir = tempfile.mkdtemp()
 filename = 'weights.mymodel.caffemodel'
